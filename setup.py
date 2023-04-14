e9b67ada816cafb96bb3ebc763bb43980f125d72
Viet-Anh <vietaing@gmail.com>
2023-04-14 10:55:36 +0100

    more cleaning

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
+ext_modules=[
+	Extension('src.girg', ['src/girg.pyx'],),
+	Extension('src.dnu', ['src/dnu.pyx'])
+]
+
 setup(
-	ext_modules=cythonize(['src/girg.pyx', 'src/dnu.pyx'], annotate=True)
+	ext_modules=cythonize(ext_modules, annotate=True)
 )
